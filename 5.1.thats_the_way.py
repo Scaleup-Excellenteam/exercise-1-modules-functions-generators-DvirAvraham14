2875e499f2228f51d81a41f76920c7c3c5ee8275
DvirAvraham14 <DvirAvraham14@gmail.com>
2023-03-16 17:21:52 +0200

    thats the way - done

--- 5.1.thats_the_way.py
+++ 5.1.thats_the_way.py
@@ -1,10 +1,21 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import constant
+import os
+
+""" 
+function that receive folder path and return all the fills
+that start with the constant {START_WITH} = "deep"
+"""
+def find_deep_files(folder_path):
+    files = [] # list to save the files name
+    for filename in os.listdir(folder_path):
+        if filename.startswith(constant.START_WITH):
+            files.append(filename)
+    return files
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    files = find_deep_files(r"/Users/dviravraham/PycharmProjects/pythonProject2/test")
+    print(files)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
